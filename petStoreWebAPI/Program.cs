9110dd13210e3f17e9a2300d779fcc72dcba5645
Tillmantaylor <Tillmant@etsu.edu>
2022-02-15 11:39:04 -0500

    gitignore fix

--- petStoreWebAPI/Program.cs
+++ petStoreWebAPI/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+//Adding Database
 builder.Services.AddDbContext<petStoreMonitoringAppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("petStoreMonitoringAppDbContext")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
