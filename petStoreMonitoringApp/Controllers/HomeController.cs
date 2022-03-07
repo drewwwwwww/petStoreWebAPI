c79ebe8fee1b85706ac0c706d8bb00808321cdbd
Tillmantaylor <Tillmant@etsu.edu>
2022-03-07 17:47:08 -0500

    Added ability to add/remove roles from a user

--- petStoreMonitoringApp/Controllers/HomeController.cs
+++ petStoreMonitoringApp/Controllers/HomeController.cs
@@ -42,6 +42,7 @@ namespace petStoreMonitoringApp.Controllers
             foreach (var user in users)
             {
                 var tempViewModel = new MaintainUsersVM();
+                tempViewModel.UserId = user.Id;
                 tempViewModel.UserName = user.UserName;
                 tempViewModel.Email = user.Email;
                 tempViewModel.Roles = await GetUserRoles(user);
@@ -54,6 +55,65 @@ namespace petStoreMonitoringApp.Controllers
         private async Task<List<string>> GetUserRoles(IdentityUser user)
         {
             return new List<string>(await _userManager.GetRolesAsync(user));
+        }
+        #endregion
+
+        #region ManageUserRolesGet
+        public async Task<IActionResult> ManageUserRoles(string userId)
+        {
+            ViewBag.userId = userId;
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"User with Id = {userId} cannot be found";
+                return View("NotFound");
+            }
+            ViewBag.UserName = user.UserName;
+            var model = new List<ManageUserRolesVM>();
+            foreach (var role in _roleManager.Roles)
+            {
+                var userRolesViewModel = new ManageUserRolesVM
+                {
+                    RoleId = role.Id,
+                    RoleName = role.Name
+                };
+                if (await _userManager.IsInRoleAsync(user, role.Name))
+                {
+                    userRolesViewModel.Selected = true;
+                }
+                else
+                {
+                    userRolesViewModel.Selected = false;
+                }
+                model.Add(userRolesViewModel);
+            }
+            return View(model);
+        }
+        #endregion
+
+        #region ManageUserRolesPost
+        [HttpPost]
+        public async Task<IActionResult> ManageUserRoles(List<ManageUserRolesVM> model, string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return View();
+            }
+            var roles = await _userManager.GetRolesAsync(user);
+            var result = await _userManager.RemoveFromRolesAsync(user, roles);
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError("", "Cannot remove user existing roles");
+                return View(model);
+            }
+            result = await _userManager.AddToRolesAsync(user, model.Where(x => x.Selected).Select(y => y.RoleName));
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError("", "Cannot add selected roles to user");
+                return View(model);
+            }
+            return RedirectToAction("Index");
         }
         #endregion
 
--- petStoreMonitoringApp/Models/ViewModels/MaintainUsersVM.cs
+++ petStoreMonitoringApp/Models/ViewModels/MaintainUsersVM.cs
@@ -4,6 +4,7 @@ namespace petStoreMonitoringApp.Models.ViewModels
 {
     public class MaintainUsersVM
     {
+        public string UserId { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
         public IEnumerable<string> Roles { get; set; }
