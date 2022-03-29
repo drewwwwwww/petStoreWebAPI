3698d3b3db08ba2e779359333722280483c785fa
Tillmantaylor <Tillmant@etsu.edu>
2022-03-29 14:20:27 -0400

    Refactored controller layout. Added User and Admin controller along with their respective functions. Moved the views to their new folders and changed buttons to route to new changes.

---
+++ petStoreMonitoringApp/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace petStoreMonitoringApp.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MetricSelect()
+        {
+            return View();
+        }
+
+        //[HttpGet("/Views/Home/ProjectInfo/Business")]
+        public IActionResult Business()
+        {
+            //return View("/Views/Home/ProjectInfo/Business"); //I wanted to get it into a subfolder but couldn't
+            return View();
+        }
+
+        //[HttpGet("/Views/Home/ProjectInfo/Performance")]
+        public IActionResult Performance()
+        {
+            //return View("/Views/Home/ProjectInfo/Performance"); //I wanted to get it into a subfolder but couldn't
+            return View();
+        }
+    }
+}
