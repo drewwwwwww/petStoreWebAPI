b49999f0f3fc81378bc782b463f28a5f19b5fffb
Brianna Martinson <martinson@etsu.edu>
2022-04-03 18:44:59 -0400

    Chart pulls from aggregated data

--- petStoreMonitoringApp/Models/ViewModels/BusinessMetricsVM.cs
+++ petStoreMonitoringApp/Models/ViewModels/BusinessMetricsVM.cs
@@ -2,9 +2,12 @@
 {
     public class BusinessMetricsVM
     {
+        //raw data
         public List<OnHandMerch> OnHandMerchList { get; set; }
         public List<AnimalPurchaseCategory> AnimalPurchaseCategoryList { get; set; }
         public List<OrderState> OrderStateList { get; set; }
+
+        //aggregated data
         public List<OnHandMerch> MostRecentOnHandMerch { get; set; }
     }
 }
