002f56bcaba4f2784313f5e2ab573481f734c7f4
Tillmantaylor <Tillmant@etsu.edu>
2022-04-03 23:10:34 -0400

    Trying to test

---
+++ PetstoreMonitoringTesting/UserControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+using petStoreMonitoringApp.Controllers;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using petStoreMonitoringApp.Models.ViewModels;
+using petStoreMonitoringApp.Models;
+
+namespace PetstoreMonitoringTesting
+{
+    [TestFixture]
+    public class UserControllerTests
+    {
+
+        [Test]
+        public void Index_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var userController = new UserController();
+
+            // Act
+            var result = userController.Business();
+
+            // Assert
+            Assert.Pass();
+        }
+
+        public class BusinessMetricsVM
+        {
+            //raw data
+            public List<OnHandMerch> OnHandMerchList { get; set; }
+            public List<AnimalPurchaseCategory> AnimalPurchaseCategoryList { get; set; }
+            public List<OrderState> OrderStateList { get; set; }
+
+            //aggregated data
+            public List<OnHandMerch> MostRecentOnHandMerch { get; set; }
+            public Dictionary<string, int> TotalAnimalsInCategoryPurchased { get; set; }
+            public Dictionary<string, int> TotalOrdersInState { get; set; }
+        }
+    }
+}
