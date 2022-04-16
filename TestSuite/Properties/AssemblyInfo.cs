78dfc47b35294bcaa0f14b9add95d221a8522c7f
Duncan <duncanrhayes@gmail.com>
2022-04-16 18:58:23 -0400

    added page id's for selenium routing to the register and login pages

---
+++ TestSuite/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestSuite")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestSuite")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("eb298456-3e17-4f85-8ade-feb1fde3caea")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/Controllers/AdminControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using Assert = NUnit.Framework.Assert;
+
+namespace UnitTestProject1.Controllers
+{
+
+
+    [TestClass]
+    public class AdminControllerTests
+    {
+        public IWebDriver Driver;
+
+        [SetUp]
+        public void SetUp() 
+        {
+            Console.WriteLine("Setup");
+            Driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void Register_User_Creates_New_User() 
+        {
+            Driver.Navigate().GoToUrl("http://monitoringmvcwebapp.azurewebsites.net/");
+
+            Assert.IsTrue(Driver.FindElement(By.ClassName("registerPage")).Displayed);
+        }
+    }
+}
