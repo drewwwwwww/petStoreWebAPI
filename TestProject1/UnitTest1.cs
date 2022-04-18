3b0c183fcfa6359d3ea6a5f79cce754e605693c0
Brianna Martinson <martinson@etsu.edu>
2022-04-17 21:01:55 -0400

    Added needed NuGet Package

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,4 +1,4 @@
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
@@ -9,18 +9,18 @@ namespace TestProject1
 
         public IWebDriver Driver;
 
-        //[SetUp]
-        //public void Setup()
-        //{
-        //    Driver = new ChromeDriver();
-        //}
+        [SetUp]
+        public void Setup()
+        {
+            Driver = new ChromeDriver();
+        }
 
-        //[Test]
-        //public void Test1()
-        //{
-        //    Driver.Navigate().GoToUrl("http://monitoringmvcwebapp.azurewebsites.net/");
+        [Test]
+        public void Test1()
+        {
+            Driver.Navigate().GoToUrl("http://monitoringmvcwebapp.azurewebsites.net/");
 
-        //    Assert.IsTrue(Driver.FindElement(By.ClassName("registerPage")).Displayed);
-        //}
+            Assert.IsTrue(Driver.FindElement(By.ClassName("registerPage")).Displayed);
+        }
     }
 }
