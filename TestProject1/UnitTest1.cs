a97a205f52e68040e522951ad9675ae00c2ec5d0
Duncan <duncanrhayes@gmail.com>
2022-04-19 12:47:22 -0400

    New Test that creates a user and verifies they are logged in

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
 
 namespace TestProject1
 {
@@ -16,11 +17,45 @@ namespace TestProject1
         }
 
         [Test]
-        public void Test1()
+        public void Register_Button_Routes_To_Correct_Page()
         {
             Driver.Navigate().GoToUrl("http://monitoringmvcwebapp.azurewebsites.net/");
 
             Assert.IsTrue(Driver.FindElement(By.Id("registerPage")).Displayed);
+        }
+
+        [Test]
+        public void Login_Button_Routes_To_Correct_Page()
+        {
+            Driver.Navigate().GoToUrl("http://monitoringmvcwebapp.azurewebsites.net/");
+
+            Assert.IsTrue(Driver.FindElement(By.Id("loginPage")).Displayed);
+        }
+
+        [Test]
+        public void Register_Page_Creates_New_User()
+        {
+            Driver.Navigate().GoToUrl("http://monitoringmvcwebapp.azurewebsites.net/Identity/Account/Register");
+
+            WebElement? emailForm = (WebElement?)(Driver.FindElement(By.Id("Input_Email")));
+
+            emailForm?.SendKeys("SelniumTest@Test.com");
+
+            WebElement? passwordForm = (WebElement?)(Driver.FindElement(By.Id("Input_Password")));
+
+            passwordForm?.SendKeys("Password1!");
+
+            WebElement? passwrodConfirmForm = (WebElement?)(Driver.FindElement(By.Id("Input_ConfirmPassword")));
+
+            passwrodConfirmForm?.SendKeys("Password1!");
+
+            WebElement? submitButton = (WebElement?)(Driver.FindElement(By.Id("registerSubmit")));
+
+            submitButton.Click();
+
+            WebElement? userNameInTopRight = (WebElement?)(Driver.FindElement(By.XPath("//a[@title='Manage']")));
+
+            Assert.IsTrue(!userNameInTopRight.Text.Equals("SelniumTest@Test.com"));
         }
     }
 }
