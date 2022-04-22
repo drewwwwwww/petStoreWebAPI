5d994ef75cd7dd84709d5df1b6157cc2df6b1341
Drew Whitinger <drew.whitinger@gmail.com>
2022-04-22 16:36:17 -0400

    Try regular way of running tests

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -22,6 +22,8 @@ namespace TestProject1
             Driver.Navigate().GoToUrl("https://localhost:5001/");
 
             Assert.IsTrue(Driver.FindElement(By.Id("registerPage")).Displayed);
+
+            Driver.Close();
         }
 
         [Test]
@@ -30,6 +32,8 @@ namespace TestProject1
             Driver.Navigate().GoToUrl("https://localhost:5001/");
 
             Assert.IsTrue(Driver.FindElement(By.Id("loginPage")).Displayed);
+
+            Driver.Close();
         }
 
         [Test]
@@ -44,6 +48,8 @@ namespace TestProject1
             button.Click();
 
             Assert.That(Driver.Url == "https://localhost:5001/Identity/Account/Login?ReturnUrl=%2FUser%2FIndex");
+
+            Driver.Close();
         }
 
         [Test]
@@ -58,6 +64,8 @@ namespace TestProject1
             button.Click();
 
             Assert.That(Driver.Url == "https://localhost:5001/Identity/Account/Login?ReturnUrl=%2FAdmin%2FIndex");
+
+            Driver.Close();
         }
 
 
@@ -86,6 +94,8 @@ namespace TestProject1
             WebElement? userNameInTopRight = (WebElement?)(Driver.FindElement(By.XPath("//a[@title='Manage']")));
 
             Assert.IsTrue(!userNameInTopRight.Text.Equals("SeleniumTest@Test.com"));
+
+            Driver.Close();
         }
 
         [Test]
@@ -117,6 +127,7 @@ namespace TestProject1
 
             deleteButton?.Click();
 
+            Driver.Close();
         }
     }
 }
